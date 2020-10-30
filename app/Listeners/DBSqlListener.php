343b9d0f2ef131cefc934ed3b1a2bbd31682c209
yefangyong <1281074511@qq.com>
2020-10-30 14:43:42 +0800

    add sql log Listener

---
+++ app/Listeners/DBSqlListener.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class DBSqlListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  QueryExecuted  $event
+     * @return void
+     */
+    public function handle(QueryExecuted $event)
+    {
+        if (env('APP_DEBUG') === true || app()->environment(['testing', 'local'])) {
+            $sql      = $event->sql;
+            $bindings = $event->bindings;
+            $time     = $event->time;
+            $bindings = array_map(function ($binding) {
+                if (is_string($binding)) {
+                    return "'$binding'";
+                } elseif ($binding instanceof \DateTime) {
+                    return $binding->format("'Y-m-d H:i:s'");
+                }
+                return $binding;
+            }, $bindings);
+            $sql      = str_replace('?', '%s', $sql);
+            $sql      = sprintf($sql, ...$bindings);
+            Log::info('sql log', ['sql' => $sql, 'time' => $time]);
+        }
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
+use App\Listeners\DBSqlListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        //注册sql执行监听器
+        QueryExecuted::class => [
+            DBSqlListener::class
+        ]
     ];
 
     /**
@@ -28,7 +33,5 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-
-        //
     }
 }
