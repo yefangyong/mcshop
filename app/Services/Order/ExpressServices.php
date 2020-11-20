48b85ac4d9f057fdd05cc4c7d59ef82cdd9c9abc
yefangyong <1281074511@qq.com>
2020-11-20 14:09:26 +0800

    + 添加查询订单物流的服务

--- app/Services/Order/ExpressServices.php
+++ app/Services/Order/ExpressServices.php
@@ -8,6 +8,8 @@ use App\Services\BaseServices;
 
 class ExpressServices extends BaseServices
 {
+
+
     public function getExpressName($code)
     {
         return [
@@ -25,5 +27,86 @@ class ExpressServices extends BaseServices
                    "FEDEX"    => "FEDEX联邦(国内件)",
                    "FEDEX_GJ" => "FEDEX联邦(国际件)",
                ][$code] ?? '';
+    }
+
+    /**
+     * @param $shipCode
+     * @param $shipSn
+     * @return string
+     * 查询订单物流轨迹
+     */
+    public function getOrderTracesByJson($shipCode, $shipSn)
+    {
+        $requestData = "{'OrderCode':'','ShipperCode':'$shipCode','LogisticCode':'$shipSn'}";
+
+        $reqUrl     = env('EXPRESS_URL');
+        $appKey     = env('EXPRESS_APP_KEY');
+        $businessId = env('EXPRESS_APP_ID');
+
+        $datas = array(
+            'EBusinessID' => $businessId,
+            'RequestType' => '1002',
+            'RequestData' => urlencode($requestData),
+            'DataType'    => '2',
+        );
+
+        $datas['DataSign'] = $this->encrypt($requestData, $appKey);
+        //根据公司业务处理返回的信息......
+
+        $res = $this->sendPost($reqUrl, $datas);
+        $res = json_decode($res, true);
+        return $res;
+    }
+
+
+    /**
+     * @param $url
+     * @param $datas
+     * @return string
+     * url响应返回的html
+     */
+    private function sendPost($url, $datas)
+    {
+        $temps = array();
+        foreach ($datas as $key => $value) {
+            $temps[] = sprintf('%s=%s', $key, $value);
+        }
+        $post_data = implode('&', $temps);
+        $url_info  = parse_url($url);
+        if (empty($url_info['port'])) {
+            $url_info['port'] = 80;
+        }
+        $httpheader = "POST " . $url_info['path'] . " HTTP/1.0\r\n";
+        $httpheader .= "Host:" . $url_info['host'] . "\r\n";
+        $httpheader .= "Content-Type:application/x-www-form-urlencoded\r\n";
+        $httpheader .= "Content-Length:" . strlen($post_data) . "\r\n";
+        $httpheader .= "Connection:close\r\n\r\n";
+        $httpheader .= $post_data;
+        $fd         = fsockopen($url_info['host'], $url_info['port']);
+        fwrite($fd, $httpheader);
+        $gets       = "";
+        $headerFlag = true;
+        while (!feof($fd)) {
+            if (($header = @fgets($fd)) && ($header == "\r\n" || $header == "\n")) {
+                break;
+            }
+        }
+        while (!feof($fd)) {
+            $gets .= fread($fd, 128);
+        }
+        fclose($fd);
+
+        return $gets;
+    }
+
+    /**
+     * @param $data
+     * @param $appkey
+     * @return string
+     * 电商Sign签名生成
+     */
+    private function encrypt($data, $appkey)
+    {
+        return urlencode(base64_encode(md5($data . $appkey)));
     }
 }
